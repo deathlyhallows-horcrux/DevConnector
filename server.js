a6fbf9158970a2ec42390d86eef1164f891a0b45
msampelli28 <meghana.sampelli@gmail.com>
2021-03-18 11:46:57 -0700

    new pages

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser'); //revisit
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express(); //creates an instance of express
 
 const users = require('./routes/api/users');
@@ -21,12 +22,21 @@ mongoose
   .then(() => console.log('MongoDb connected'))
   .catch(err => console.log(err));
 
-//first route --contains request and response 
-app.get('/', (req, res) => {
-  res.send("Hello World, Meghana");
-});
+// //first route --contains request and response 
+// app.get('/', (req, res) => {
+//   res.send("Hello World, Meghana");
+// });
 
 app.use('/api/users', users);
 
-const port = 9000;
+if(process.env.NODE_ENV === 'production'){
+  //in prod when user hits url askin express to use the build folder
+    app.use(express.static('client/build'));
+    //whatever url the user comes to asking to go to where the current application is deployed at this path, without needing CRA at the cloud 
+    app.get('*', (req , res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+const port = process.env.PORT || 9000;
 app.listen(port, () => console.log(`server is running on the port ${port}`));
