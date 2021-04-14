36a77d9bbb2e57202d9cd9fc407f1556667d20eb
msampelli28 <meghana.sampelli@gmail.com>
2021-04-14 14:50:23 -0700

    041421

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,3 @@
-// module.exports = {
-//   mongoURI : 'mongodb+srv://kaladmin:J0UFBrn6FOz4inEz@devconnector.f40on.mongodb.net/devconnectordb?retryWrites=true&w=majority',
-//   secretOrKey: 'secret'
-// }
-
 if(process.env.NODE_ENV === 'production'){
   module.exports = require('./keys_prod');
 }else{
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI : 'mongodb+srv://kaladmin:J0UFBrn6FOz4inEz@devconnector.f40on.mongodb.net/devconnectordb?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
+}
